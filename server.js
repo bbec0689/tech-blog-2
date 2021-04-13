d839c6791094a05c17f65e6c522d187a852fed5b
Dave <bbec0689@gmail.com>
2021-04-13 13:01:56 -0600

    heroku errors

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ const sess = {
 };
 
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 3030;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
